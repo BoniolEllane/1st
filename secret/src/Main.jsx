1c167c825e18b8e36c2fe18e8e122822ec9e7292
2916lalala <74847517+BoniolEllane@users.noreply.github.com>
2024-09-11 13:58:50 +0800

    r

--- secret/src/Main.jsx
+++ secret/src/Main.jsx
@@ -4,12 +4,22 @@ import React from 'react';
 function Main() {
   return (
     <div className="main">
-      <div className="rowcon">
-        <div className="concol">
-          
+      <div className="concol">
+        <div className="rowcon">
+          <div className="concol">
+            <p>sample</p>
+            <h1>secure</h1>
+            <p>hed ut</p>
+          </div>
         </div>
-        <div className="concol">
-
+        <div className="rowcon">
+          <div className="rowbet">
+            <button>live</button>
+            <div className="col">
+              <p>reality</p>
+              <p>quis</p>
+            </div>
+          </div>
         </div>
       </div>
     </div>
