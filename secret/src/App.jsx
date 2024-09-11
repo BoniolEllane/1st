85e5834bb9dfb85aba19e273e68838a19baf812b
2916lalala <74847517+BoniolEllane@users.noreply.github.com>
2024-09-11 13:28:41 +0800

    f

---
+++ secret/src/App.jsx
@@ -0,0 +1,19 @@
+import './index.css';
+import React from 'react';
+
+function App() {
+  return (
+    <div className="main">
+      <div className="rowcon">
+        <div className="concol">
+          
+        </div>
+        <div className="concol">
+
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
