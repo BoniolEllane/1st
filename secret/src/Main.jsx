4f1296893b551c8293a33e313f54cac8554f28db
2916lalala <74847517+BoniolEllane@users.noreply.github.com>
2024-09-20 13:56:46 +0800

    d

--- secret/src/Main.jsx
+++ secret/src/Main.jsx
@@ -107,18 +107,18 @@ function Main() {
 
       <section className="dedicat">
         <div className="rowd">
-          <div className='colcon'>
+          <div className='dedi'>
             <p>FAFANA</p>
             <h3>NFJNJANIONFV</h3>
             <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit,
               sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
               Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</p>
           </div>
-          <div className='colcon'>
-            <img src={image4} alt="" />
-            <img src={smolim3} alt="" />
+          <div className='imggr2'>
+            <img className='big4' src={image4} alt="" />
+            <img className='sm3' src={smolim3} alt="" />
           </div>
-          <div className='colcon'>
+          <div className='colcon2'>
             <ul className="bullet">
               <li>MKDMFCKAMN</li>
               <li>MKDMFCKAMN</li>
